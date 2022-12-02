835b857e7e7b2f30039bfdd633cb3c2bc5a29e02
sahanuser <sahan.user@gmail.com>
2022-12-02 22:04:17 +0530

    repo

---
+++ x86_64/repush.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -r aster-linux-repo*
+repo-add -v --verify --sign aster-linux-repo.db.tar.gz *.pkg.tar.zst
+rm -r aster-linux-repo.db
+rm -r  aster-linux-repo.files
+mv aster-linux-repo.db.tar.gz aster-linux-repo.db
+mv aster-linux-repo.files.tar.gz aster-linux-repo.files
+
+git add .
+git commit  -m "repo" 
+git push --set-upstream origin master
+
